d45508e328ab137649825199c486bca2e134749d
DIEGO <diegoh_c@yahoo.com>
2016-04-11 21:09:15 -0700

    profile, home and sign in, sign up finished

--- app/controllers/initial_controller.rb
+++ app/controllers/initial_controller.rb
@@ -4,7 +4,7 @@ class InitialController < ApplicationController
     if user_signed_in?
       redirect_to books_path
     else
-      redirect_to new_user_session_path
+      redirect_to new_user_registration_path
     end
   end
 
