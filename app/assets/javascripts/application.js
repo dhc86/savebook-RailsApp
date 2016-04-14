07fb0b36286196688235cfa8116e9d3ba3ebb747
DIEGO <diegoh_c@yahoo.com>
2016-04-14 14:01:02 -0700

    added small extra validation to isbn input

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,36 +26,42 @@ $( document ).ready(function() {
   $('.find-isbn-button').on('click', function(event){
     event.preventDefault();
 
-    var isbn = $('#isbn-entered').val();
+    if ($('#isbn-entered').val() == ""){
+      alert("Plase enter an isbn number!");
+    }
+    else{
+      var isbn = $('#isbn-entered').val();
 
-    $.ajax({
-      url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:'+isbn,
-      method: 'GET',
-      dataType: 'jsonp',
-      success: function(data){
-        // debugger
-        if (data.totalItems == 0){
-          alert("Please enter a valid isbn, book not found.");
-        }
-        else {
-          title = data["items"][0]["volumeInfo"]["title"];
-          author = data["items"][0]["volumeInfo"]["authors"][0];
-          description = data["items"][0]["volumeInfo"]["description"];
-          if (data["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"] == null){
-            picture_url = 'http://i.imgur.com/Ibd6JdC.jpg'
+      $.ajax({
+        url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:'+isbn,
+        method: 'GET',
+        dataType: 'jsonp',
+        success: function(data){
+          // debugger
+          if (data.totalItems == 0){
+            alert("Please enter a valid isbn, book not found.");
           }
-          else{
-            picture_url = data["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]; 
-          }   
-          $('.book_title input').val(title);
-          $('.book_author input').val(author);
-          $('.book_isbn input').val(isbn);
-          $('.book_description textarea').val(description);
-          $('.book_picture_url input').val(picture_url);
-          debugger
+          else {
+            title = data["items"][0]["volumeInfo"]["title"];
+            author = data["items"][0]["volumeInfo"]["authors"][0];
+            description = data["items"][0]["volumeInfo"]["description"];
+            if (data["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"] == null){
+              picture_url = 'http://i.imgur.com/Ibd6JdC.jpg'
+            }
+            else{
+              picture_url = data["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]; 
+            }   
+            $('.book_title input').val(title);
+            $('.book_author input').val(author);
+            $('.book_isbn input').val(isbn);
+            $('.book_description textarea').val(description);
+            $('.book_picture_url input').val(picture_url);
+            debugger
+          }
         }
-      }
-    });
+      });      
+    }
+
 
 
 
